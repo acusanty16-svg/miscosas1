39f3de176028e9ed7f9859b88065b227eceffe34
santiagoacuña1 <acusanty16@gmail.com>
2025-10-23 10:27:45 +0200

    update

--- proyecto_Inicial/src/Main.java
+++ proyecto_Inicial/src/Main.java
@@ -1,2 +1,5 @@
 public class Main {
-}
+    public static void main(String[] args){
+ System.out.println("este es el primer proyecto que realizare.\ny que pasa si hablo aquí    \ny ahora");
+    }
+    }
