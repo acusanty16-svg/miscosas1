c8f93c3549ea8285331c1edd48aa6c30c8420cae
santiagoacuña1 <acusanty16@gmail.com>
2025-10-23 12:19:43 +0200

    Update Main.java

--- proyecto_Inicial/src/Main.java
+++ proyecto_Inicial/src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
-    public static void main(String[] args){
- System.out.println("este es el primer proyecto que realizare.\ny que pasa si hablo aquí    \ny ahora");
+public class Main{
+
+    public static void main(String[]args){
+
+        System.out.print("Partir\nen cuerpo y alma\npartir\n\nPartir\ndsahacerse de las miradas\npiedras opresoras\nque duermen en la garganta\n\nHe de partir\nno más inercia bajo el sol\nno más sangre anodada\n\nHe de partir\n\nPero arremete !viajera¡");
     }
-    }
+
+}
