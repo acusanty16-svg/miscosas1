8fbe2b3f81c31f7b5f7d72b34913df90c08263bb
santiagoacuña1 <acusanty16@gmail.com>
2025-10-25 11:02:19 +0200

    update

--- proyecto_Inicial/src/Main.java
+++ proyecto_Inicial/src/Main.java
@@ -5,4 +5,6 @@ public class Main{
         System.out.print("Partir\nen cuerpo y alma\npartir\n\nPartir\ndsahacerse de las miradas\npiedras opresoras\nque duermen en la garganta\n\nHe de partir\nno más inercia bajo el sol\nno más sangre anodada\n\nHe de partir\n\nPero arremete !viajera¡");
     }
 
+
+
 }
