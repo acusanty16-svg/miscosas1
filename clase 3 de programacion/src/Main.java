696930e08aacd236c18bf30aceccc338d02e7258
santiagoacuña1 <acusanty16@gmail.com>
2025-10-29 12:56:48 +0100

    update

--- clase 3 de programacion/src/Main.java
+++ clase 3 de programacion/src/Main.java
@@ -5,6 +5,7 @@ public class Main{
 
         System.out.print("Santiago");
 
+
     }
 
 }
--- proyecto_Inicial/src/Main.java
+++ proyecto_Inicial/src/Main.java
@@ -1,10 +1,39 @@
 public class Main{
-
     public static void main(String[]args){
+        System.out.print("Partir\nen cuerpo y alma\npartir\n\nPartir\ndsahacerse de las miradas\npiedras opresoras\nque duermen en la garganta\n\nHe de partir\nno más inercia bajo el sol\nno más sangre anodada\n\nHe de partir\n\nPero arremete !viajera¡\n");
 
-        System.out.print("Partir\nen cuerpo y alma\npartir\n\nPartir\ndsahacerse de las miradas\npiedras opresoras\nque duermen en la garganta\n\nHe de partir\nno más inercia bajo el sol\nno más sangre anodada\n\nHe de partir\n\nPero arremete !viajera¡");
-    }
+//este es un comentario tipo 1 que explico el profe en clase
+
+        /*este es otro tipo de comentario que podriamos
+        decir y definir como el tipo 2
+         */
+/**
+ * este ya es el tercer tipo de comentario
+ * que mas o menos funciona como una biblioteca o ayuda
+ * donde puedo poner algo de informacion base
+ * @author por ejemplo
+ * @main por ejemplo. dependiendo de la necesidad de mi comentario
+ */
+//TODO este es el comentario que mas me gusto en lo personal
+        //TODO porque en este se pueden dejar tareas pendientes para realizar en un futuro
 
 
+        String nombreCompleto = "Santiago Acuna"; //null, es decir que es nulo. los Strings son palabra y van entrecomillados dobles
+        nombreCompleto = "Santiago A.";
+        int edad = 24;
+        edad = 23 + 1;
+        float altura = 1.74f;
+        double peso = 85.3242354;
+        char letraCarnet = 'K';
+        boolean experiencia = true;
+        experiencia = false;
 
+        System.out.println("Mi nombre es: "+nombreCompleto);
+        System.out.println("Mi edad es: "+edad+" años.");
+        System.out.println("Mi altura es: "+altura+" cm.");
+        System.out.println("Mi peso es: "+peso+" kg.");
+        System.out.println("La letra de mi DNI: "+letraCarnet+".");
+        System.out.println("tengo experiencia: "+experiencia);
+
+}
 }
